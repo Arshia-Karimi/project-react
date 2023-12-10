6b9b6114980c5f94aa7b9e533a5fd6f2e40125ab
arshia <arshia.karimi@gmail.com>
2023-12-10 17:08:58 +0330

    fed

---
+++ src/data/products.js
@@ -0,0 +1,37 @@
+import mobile from "../assets/Mobile.jpg";
+import laptop from "../assets/laptop.jpg";
+import camera from "../assets/camera.jpg";
+import denim from "../assets/denim.jpeg";
+import shoes from "../assets/shoes.jpg";
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Mobile",
+    price: 1000,
+    productImage: mobile,
+  },
+  {
+    id: 1,
+    productName: "laptop",
+    price: 2000,
+    productImage: laptop,
+  },
+  {
+    id: 1,
+    productName: "camera",
+    price: 1100,
+    productImage: camera,
+  },
+  {
+    id: 1,
+    productName: "denim",
+    price: 200,
+    productImage: denim,
+  },
+  {
+    id: 1,
+    productName: "shoes",
+    price: 100,
+    productImage: shoes,
+  },
+];
--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import Product from "./product.js";
+import { PRODUCTS } from "../../data/products";
+
 const Shop = () => {
   return (
     <div>
       <h1>Shop</h1>
+      <div className="row">
+        {PRODUCTS.map((productDate) => {
+          return <Product key={productDate.id} data={productDate} />;
+        })}
+      </div>
     </div>
   );
 };
